167f14dd77d69e4e278b022f028bbe42c1b23b82
April Peng and Vanessa Palacios <vanelunapalacios@live.com>
2016-06-27 20:07:24 -0700

    create js logic for business and user interface. create form and other content in index.html. create css to hide sections until submit button pressed as well as other customizations to style

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+//business logic
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  //this.street = street;
+  //this.street = city;
+  //this.street = state;
+}
+
+Contact.prototype.fullName = function () {
+  return this.firstName + " " + this.lastName;
+};
+
+//user interface logic
+$(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact (inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
+  });
+});
